7551bf66f63c9ace372124b5442d054a1740a9cc
Phyo Wai Win <85800387+phyoww@users.noreply.github.com>
2023-06-09 11:03:11 +0800

    Update firewall-rule.tf

--- firewall-rule.tf
+++ firewall-rule.tf
@@ -1,19 +1,50 @@
-resource "aws_networkfirewall_rule_group" "allow-local" {
-  capacity = 1000
-  name     = "allow-local-ranges"
+#resource "aws_networkfirewall_rule_group" "allow-local" {
+#  capacity = 1000
+#  name     = "allow-local-ranges"
+#  type     = "STATELESS"
+#  rule_group {
+#    rules_source {
+#      stateless_rules_and_custom_actions {
+#        stateless_rule {
+#          priority = 5
+#          rule_definition {
+#            actions = ["aws:pass"]
+#              source {
+#            match_attributes {
+#                address_definition = "0.0.0.0/0"
+#              }
+#              source {
+#                address_definition = "192.168.0.0/16"
+#              }
+#              destination {
+#                address_definition = "10.0.1.0/24"
+#              }
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
+
+resource "aws_networkfirewall_rule_group" "drop_icmp_traffic_fw_rule_group" {
+  name     = "drop-icmp-traffic-fw-rule-group"
+  capacity = 100
   type     = "STATELESS"
+
   rule_group {
     rules_source {
       stateless_rules_and_custom_actions {
         stateless_rule {
-          priority = 5
+          priority = 1
           rule_definition {
-            actions = ["aws:pass"]
+            actions = ["aws:drop"]
             match_attributes {
+              protocols = [1]
               source {
-                address_definition = "10.0.0.0/8"
+                address_definition = "0.0.0.0/0"
               }
-              source {
+              destination {
                 address_definition = "10.0.1.0/24"
               }
             }
@@ -22,73 +53,103 @@ resource "aws_networkfirewall_rule_group" "allow-local" {
       }
     }
   }
+
 }
 
-resource "aws_networkfirewall_rule_group" "deny-http-domains" {
+resource "aws_networkfirewall_rule_group" "pass_ssh_traffic_fw_rule_group" {
+  name     = "pass-ssh-traffic-fw-rule-group"
   capacity = 100
-  name     = "deny-http-domains"
-  type     = "STATEFUL"
+  type     = "STATELESS"
+
   rule_group {
     rules_source {
-      rules_source_list {
-        generated_rules_type = "DENYLIST"
-        target_types         = ["HTTP_HOST"]
-        targets              = var.http-domains-to-deny
-      }
-    }
-  }
-
-  tags = {
-    "Name" = "deny-http-domains"
-  }
-}
-
-resource "aws_networkfirewall_rule_group" "deny-https-domains" {
-  capacity = 100
-  name     = "deny-https-domains"
-  type     = "STATEFUL"
-  rule_group {
-    rules_source {
-      rules_source_list {
-        generated_rules_type = "DENYLIST"
-        target_types         = ["TLS_SNI"]
-        targets              = var.https-domains-to-deny
-      }
-    }
-  }
-
-  tags = {
-    "Name" = "deny-https-domains"
-  }
-}
-
-resource "aws_networkfirewall_rule_group" "deny-http" {
-  capacity = 100
-  name     = "deny-http"
-  type     = "STATEFUL"
-  rule_group {
-    rules_source {
-      stateful_rule {
-        action = "DROP"
-        header {
-          destination      = aws_subnet.cloudideastar_public_subnet.cidr_block
-          destination_port = 80
-          direction        = "ANY"
-          protocol         = "HTTP"
-          source           = "0.0.0.0/0"
-          source_port      = 80
-        }
-        rule_option {
-          keyword = "sid:1"
+      stateless_rules_and_custom_actions {
+        stateless_rule {
+          priority = 2
+          rule_definition {
+            actions = ["aws:pass"]
+            match_attributes {
+              protocols = [22]
+              source {
+                address_definition = "0.0.0.0/0"
+              }
+              destination {
+                address_definition = "10.0.1.0/24"
+              }
+            }
+          }
         }
       }
     }
   }
 
-  tags = {
-    "Name" = "deny-http"
-  }
 }
+
+#resource "aws_networkfirewall_rule_group" "deny-http-domains" {
+#  capacity = 100
+#  name     = "deny-http-domains"
+#  type     = "STATEFUL"
+#  rule_group {
+#    rules_source {
+#      rules_source_list {
+#        generated_rules_type = "DENYLIST"
+#        target_types         = ["HTTP_HOST"]
+#        targets              = var.http-domains-to-deny
+#      }
+#    }
+#  }
+
+# tags = {
+#    "Name" = "deny-http-domains"
+#  }
+#}
+
+#resource "aws_networkfirewall_rule_group" "deny-https-domains" {
+#  capacity = 100
+#  name     = "deny-https-domains"
+#  type     = "STATEFUL"
+#  rule_group {
+#    rules_source {
+#      rules_source_list {
+#        generated_rules_type = "DENYLIST"
+#        target_types         = ["TLS_SNI"]
+#        targets              = var.https-domains-to-deny
+#      }
+#    }
+#  }
+
+#  tags = {
+#    "Name" = "deny-https-domains"
+#  }
+#}
+
+#resource "aws_networkfirewall_rule_group" "deny-http" {
+#  capacity = 100
+#  name     = "deny-http"
+#  type     = "STATEFUL"
+#  rule_group {
+#    rules_source {
+#      stateful_rule {
+#        action = "DROP"
+#        header {
+#          destination      = aws_subnet.application.cidr_block
+#          destination_port = 80
+#          direction        = "ANY"
+#          protocol         = "HTTP"
+#          source           = "0.0.0.0/0"
+#          source_port      = 80
+#       }
+#        rule_option {
+#          keyword = "sid:1"
+#        }
+#      }
+#    }
+#  }
+
+#  tags = {
+#    "Name" = "deny-http"
+#  }
+#}
 
 
 resource "aws_networkfirewall_rule_group" "deny-ssh" {
