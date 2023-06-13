885dd059a53af6682b89908f01116324d70de3cc
Phyo Wai Win <85800387+phyoww@users.noreply.github.com>
2023-06-13 21:41:22 +0800

    Update aws_instance.tf

--- aws_instance.tf
+++ aws_instance.tf
@@ -19,4 +19,26 @@ resource "aws_instance" "web_instance" {
   tags = {
     "Name" : "Cloudideastar"
   }
+  # Installing required softwares into the system!
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("/testkeypair.pem") #paste your xxx.pem file patch from your pc
+    host = aws_instance.web_instance.public_ip
+  }
+
+  # Code for installing the softwares!
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum update -y",
+        "sudo yum install php php-mysqlnd httpd -y",
+        "wget https://wordpress.org/wordpress-4.8.14.tar.gz",
+        "tar -xzf wordpress-4.8.14.tar.gz",
+        "sudo cp -r wordpress /var/www/html/",
+        "sudo chown -R apache.apache /var/www/html/",
+        "sudo systemctl start httpd",
+        "sudo systemctl enable httpd",
+        "sudo systemctl restart httpd"
+    ]
+  }    
 }
